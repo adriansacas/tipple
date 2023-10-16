db2056737dd3ef4905027d8e88776068a90e13f4
Claudia Castillo <claudia@Claudias-MacBook-Pro.local>
2023-10-16 01:11:34 -0500

    part of previous sessions, checkpoint

---
+++ Tipple/DateCell.swift
@@ -0,0 +1,15 @@
+//
+//  DateCell.swift
+//  Tipple
+//
+//  Created by Claudia Castillo on 10/14/23.
+//
+
+import Foundation
+import UIKit
+
+class DateCell: UICollectionViewCell {
+    
+    @IBOutlet weak var dayOfMonth: UILabel!
+    
+}
