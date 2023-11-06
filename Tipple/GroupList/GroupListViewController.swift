d089686d6bb802ae770cbcbf7d486f5b71462879
Claudia Castillo <claudia@Claudias-MBP.grandenetworks.net>
2023-11-05 23:27:06 -0600

    new files for group list

---
+++ Tipple/GroupList/GroupListViewController.swift
@@ -0,0 +1,31 @@
+//
+//  GroupListViewController.swift
+//  Tipple
+//
+//  Created by Claudia Castillo on 11/5/23.
+//
+
+import UIKit
+
+class GroupListViewController: UIViewController {
+
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Tipple/GroupList/MemberInfoViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MemberInfoViewController.swift
+//  Tipple
+//
+//  Created by Claudia Castillo on 11/5/23.
+//
+
+import UIKit
+
+class MemberInfoViewController: UIViewController {
+
+    @IBOutlet weak var nameField: UILabel!
+    @IBOutlet weak var tableView: UITableView!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
