f6ba7ae01c585e69d736e773eb1047b942024328
Claudia Castillo <claudia@wireless-10-146-207-143.public.utexas.edu>
2023-11-07 16:44:56 -0600

    setting profile pic in group list

--- Tipple/GroupList/MemberInfoViewController.swift
+++ Tipple/GroupList/MemberInfoViewController.swift
@@ -10,6 +10,7 @@ import FirebaseAuth
 
 class MemberInfoViewController: UIViewController, UITableViewDelegate, UITableViewDataSource {
 
+    @IBOutlet weak var profilePic: UIImageView!
     @IBOutlet weak var nameField: UILabel!
     @IBOutlet weak var tableView: UITableView!
     
@@ -25,9 +26,15 @@ class MemberInfoViewController: UIViewController, UITableViewDelegate, UITableVi
         
         tableView.delegate = self
         tableView.dataSource = self
+        
+        self.profilePic.layer.masksToBounds = false
+        self.profilePic.layer.cornerRadius = profilePic.frame.size.height / 2
+        self.profilePic.clipsToBounds = true
 
         for key in user!.keys {
-            if(key != "name") {
+            if(key == "Profile Pic") {
+                setProfileImage(url: user![key] as? String)
+            } else if(key != "name") {
                 keys.append(key)
             }
         }
@@ -50,6 +57,29 @@ class MemberInfoViewController: UIViewController, UITableViewDelegate, UITableVi
         cell.valueField.text = user![key] as? String
         
         return cell
+    }
+    
+    func setProfileImage(url:String?) {
+        guard let imageURLString = url,
+              let imageURL = URL(string: imageURLString) else {
+            return
+        }
+
+        // Create a URLSession data task to download the image
+        URLSession.shared.dataTask(with: imageURL) { [weak self] (data, response, error) in
+            if let error = error {
+                print("Error downloading image: \(error.localizedDescription)")
+                return
+            }
+
+            // Ensure that the downloaded data is an image
+            if let data = data, let image = UIImage(data: data) {
+                // Update the profileImage UIImageView on the main thread
+                DispatchQueue.main.async {
+                    self?.profilePic.image = image
+                }
+            }
+        }.resume()
     }
 
 }
